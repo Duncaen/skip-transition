0819f5751175607ede7607f6be307473812dee5d
Duncan Overbruck <overbruck@mir.de>
2014-10-14 12:50:56 +0200

    test

--- index.js
+++ index.js
@@ -7,19 +7,12 @@ module.exports = skipTransition;
 
 /**
  *
- * @param {Element} el
- * @param {Boolean} childs (optional)
+ * @param {Element|Array} el
  * @param {Function} fn
  * @api public
  */
-function skipTransition(el, childs, fn) {
-  if(typeof childs === 'function') {
-    fn = childs;
-    childs = undefined;
-  }
-  if(childs !== false) {
-    el = [].slice.call(el.getElementsByTagName('*'));
-  }
+function skipTransition(el, fn) {
+  if(!Array.isArray(el)) el = [el];
   setTransitionProperty(el, 'none');
   raf(
     fn.bind(null, function() {
@@ -33,16 +26,12 @@ function skipTransition(el, childs, fn) {
  *
  *
  */
-function setTransitionProperty(el, value) {
-  if(Array.isArray(el)) {
-    for(var i = 0, len = el.length; i < len; i++) {
-      setTransitionProperty(el[i], value);
-    }
-    return;
-  }
-  if(!el || !el.style) return;
-  el.style.webkitTransitionProperty =
-  el.style.mozTransitionProperty =
-  el.style.msTransitionProperty =
-  el.style.oTransitionProperty = value;
+function setTransitionProperty(els, value) {
+  els.forEach(function(el, idx, arr) {
+    if(!el || !el.style) return;
+    el.style.webkitTransitionProperty =
+    el.style.mozTransitionProperty =
+    el.style.msTransitionProperty =
+    el.style.oTransitionProperty = value;
+  });
 }
