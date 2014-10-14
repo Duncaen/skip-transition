72d10f4adfc285edc987af8def7d48e1ae52a7fa
Duncan Overbruck <overbruck@mir.de>
2014-10-14 13:01:16 +0200

    fix

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ function skipTransition(el, fn) {
  *
  */
 function setTransitionProperty(el, value) {
-  var els = [el];
-  els.push([].slice.call(el.getElementByTagName('*')));
+  var els = [el].concat([].slice.call(el.getElementByTagName('*')));
   els.forEach(function(el, idx, arr) {
     if(!el || !has(el)) return;
     el.style.webkitTransitionProperty =
